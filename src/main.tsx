9ac956d764dcbe2039ba44704d36fd93c168cdc9
Joellindberg1 <Joellindberg23@gmail.com>
2025-05-06 15:31:10 +0200

    reset & main scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.scss'
+import '@/styles/reset.scss';
+import '@/styles/main.scss';
 import App from './App.tsx'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
