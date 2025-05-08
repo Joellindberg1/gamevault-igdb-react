b017b9f299fb458811938525f0409b8bb4d7d59d
Joellindberg1 <Joellindberg23@gmail.com>
2025-05-08 13:27:56 +0200

    Nav UI fix

--- src/components/Header/NavBar.tsx
+++ src/components/Header/NavBar.tsx
@@ -4,9 +4,31 @@ import styles from './Header.module.scss';
 export default function NavBar() {
   return (
     <nav className={styles.nav}>
-      <NavLink to="/" className={styles.link}>Home</NavLink>
-      <NavLink to="/search" className={styles.link}>Search</NavLink>
-      <NavLink to="/mygames" className={styles.link}>My Games</NavLink>
+      <NavLink
+        to="/"
+        end
+        className={({ isActive }) =>
+          isActive ? `${styles.link} ${styles.activeLink}` : styles.link
+        }
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/search"
+        className={({ isActive }) =>
+          isActive ? `${styles.link} ${styles.activeLink}` : styles.link
+        }
+      >
+        Search
+      </NavLink>
+      <NavLink
+        to="/mygames"
+        className={({ isActive }) =>
+          isActive ? `${styles.link} ${styles.activeLink}` : styles.link
+        }
+      >
+        My Games
+      </NavLink>
     </nav>
   );
 }
